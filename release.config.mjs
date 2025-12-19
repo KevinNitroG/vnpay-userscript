924426878b5de9ccc02461026553bb0d43954749
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-12-19 18:46:35 +0700

    chore: wow it's... let's use semantic release

---
+++ release.config.mjs
@@ -0,0 +1,41 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "pnpm run build",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: "dist/@KevinNitroG/vnpay-userscript.user.js",
+            label: "Userscript",
+          },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/git",
+  ],
+  preset: "conventionalcommits",
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import monkey from "vite-plugin-monkey";
+import pkg from "./package.json" assert { type: "json" };
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,12 +10,16 @@ export default defineConfig({
     monkey({
       entry: "src/main.tsx",
       userscript: {
-        icon: "https://vitejs.dev/logo.svg",
+        author: "Kevin Nitro",
         namespace: "kevinnitrog/vnpay-userscript",
+        iconURL: "https://sandbox.vnpayment.vn/merchantv2/favicon.ico",
+        downloadURL:
+          "https://github.com/KevinNitroG/vnpay-userscript/releases/latest/download/vnpay-userscript.user.js",
+        defaulticon: "https://sandbox.vnpayment.vn/merchantv2/favicon.ico",
+        name: "VNPay Auto Fill Userscript",
         match: ["https://sandbox.vnpayment.vn/*"],
         "run-at": "document-idle",
-        grant: "*",
-        version: "0.1.8",
+        version: pkg.version,
       },
     }),
   ],
